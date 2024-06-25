793b06952e2d2c90741cd726cdd1a2c579bec6dc
thonghtph43253 <hoangthong2105@gmail.com>
2024-06-26 00:05:49 +0700

    ok

--- FShoes/src/test/java/com/example/fshoes/controduoc/helloae.java
+++ FShoes/src/test/java/com/example/fshoes/controduoc/helloae.java
@@ -3,5 +3,6 @@ package com.example.fshoes.controduoc;
 public class helloae {
     public static void main(String[] args) {
         System.out.println("Hello, AE!");
+        //System.out.println("Hello, AE!");
     }
 }
