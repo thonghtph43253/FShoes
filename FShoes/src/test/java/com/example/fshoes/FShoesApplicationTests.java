650e237a4698e344c327e6a75c32a617276910a3
KinggKong <134825851+KinggKong@users.noreply.github.com>
2024-06-25 23:44:21 +0700

    Innit Project

---
+++ FShoes/src/test/java/com/example/fshoes/FShoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fshoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FShoesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
